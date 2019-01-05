f09bb248288bbb39511a9d81869a22dc8e58e03e
hiteshsardana <hiteshsardana99@gmail.com>
2019-01-05 18:05:04 +0530

    git connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public");
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection' , (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect' , () => {
+      console.log('disconnected from client');
+  });
+})
+
 
 app.use(express.static(publicPath));
 
@@ -11,4 +24,4 @@ console.log(publicPath);
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`server is running at ${port}`);});
+server.listen(port, () => {console.log(`server is running at ${port}`);});
